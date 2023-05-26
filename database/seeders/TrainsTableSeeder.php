b8a024a4abfebc83b45fd7cbce1d804a28f5c5ba
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-26 11:20:07 +0200

    Refactoring

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,11 +19,11 @@ class TrainsTableSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
             $new_train = new Train();
             $new_train->company = $faker->company();
-            $new_train->departure_station = $faker->word();
-            $new_train->arrival_station = $faker->word();
-            $new_train->departures_date = $faker->dateTimeInInterval("-3 days", "+6 days");
-            $new_train->departure_time = $faker->time();
-            $new_train->arrival_time = $faker->time();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departures_date = $faker->dateTimeInInterval("-2 days", "+4 days");
+            $new_train->departure_time = $faker->time("H:i:s");
+            $new_train->arrival_time = $faker->time("H:i:s");
             $new_train->train_code = $faker->randomNumber(4, true);
             $new_train->carriages_number = $faker->numberBetween(5, 15);
             $new_train->on_time = $faker->boolean();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row row-cols-auto">
             @foreach ($trains as $train)
                 <div class="col">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card my-3">
                         <div class="card-body">
                             <h5 class="card-title">{{ $train->company }}</h5>
                             <p class="card-text">Stazione di partenza: {{ $train->departure_station }}</p>
